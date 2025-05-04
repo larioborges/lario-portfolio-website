91fb2c676b5d7f9ed6bf746ec3cb83abcfd184b5
Lario Borges <lario.borges@gmail.com>
2025-05-05 00:16:36 +0200

    init theme

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -4,14 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
-import compressor from "astro-compressor";
-
+// import compressor from "astro-compressor";
+// 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://astro.lariocpt.biz',
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [sitemap(), partytown(), react(),compressor()],
+    integrations: [sitemap(), partytown(), react()/*,compressor()*/],
 
 });
--- packages/strapi/src/admin/vite.config.ts
+++ packages/strapi/src/admin/vite.config.ts
@@ -8,5 +8,8 @@ export default (config: UserConfig) => {
         '@': '/src',
       },
     },
+    esbuild: {
+      loader: { ".js": "jsx" },
+    },
   });
 };
