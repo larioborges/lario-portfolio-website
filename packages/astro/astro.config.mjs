f5745e638e55882923dae95a82fd9a7a011396e9
Lario Borges <lario.borges@gmail.com>
2025-05-04 22:59:58 +0200

    strapi and astro

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://astro.lariocpt.biz',
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [sitemap(), partytown(), react(),compressor()],
+
 });
